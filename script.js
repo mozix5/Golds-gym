65d35b078753960a9fd2e71ed6d2f77499de9b93
mozix5 <76263118+mozix5@users.noreply.github.com>
2022-11-24 01:30:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+// const counters = document.querySelectorAll('.value');
+// const speed = 200;
+
+// counters.forEach( counter => {
+//    const animate = () => {
+//       const value = +counter.getAttribute('akhi');
+//       const data = +counter.innerText;
+     
+//       const time = value / speed;
+//      if(data < value) {
+//           counter.innerText = Math.ceil(data + time);
+//           setTimeout(animate, 2);
+//         }else{
+//           counter.innerText = value;
+//         }
+     
+//    }
+   
+//    animate();
+// });
+function animate(obj, initVal, lastVal, duration) {
+    let startTime = null;
+
+ //get the current timestamp and assign it to the currentTime variable
+ let currentTime = Date.now();
+
+ //pass the current timestamp to the step function
+ const step = (currentTime ) => {
+
+ //if the start time is null, assign the current time to startTime
+ if (!startTime) {
+    startTime = currentTime ;
+ }
+
+ //calculate the value to be used in calculating the number to be displayed
+ const progress = Math.min((currentTime - startTime)/ duration, 1);
+
+ //calculate what to be displayed using the value gotten above
+ obj.innerHTML = Math.floor(progress * (lastVal - initVal) + initVal);
+
+ //checking to make sure the counter does not exceed the last value (lastVal)
+ if (progress < 1) {
+    window.requestAnimationFrame(step);
+ } else {
+       window.cancelAnimationFrame(window.requestAnimationFrame(step));
+    }
+ };
+ //start animating
+    window.requestAnimationFrame(step);
+ }
+ let text1 = document.getElementById('0101');
+ let text2 = document.getElementById('0102');
+ let text3 = document.getElementById('0103');
+ const load = () => {
+    animate(text1, 0, 511, 7000);
+    animate(text2, 0, 232, 7000);
+    animate(text3, 100, 25, 7000);
+ }
